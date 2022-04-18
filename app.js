2533565cd806bd2da91785f6b1e82d4ff8256d3d
TchalyL <leandretchaly@gmail.com>
2022-04-18 17:03:16 -0400

    Rendering view

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const PORT = 3000
 // Register view engine
 app.set('view engine', 'ejs')
 
+// Render views
+app.get('/', (req, res) => {
+  res.render('/index')
+})
+
 app.listen(PORT, ()=> {
   console.log(`Listening on port ${PORT}...`)
 })
