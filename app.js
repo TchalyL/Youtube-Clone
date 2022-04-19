c217fda51f4e348d92c7586806c037877e6619cc
TchalyL <leandretchaly@gmail.com>
2022-04-18 22:19:41 -0400

    Adding more details to be rendered

--- app.js
+++ app.js
@@ -16,22 +16,34 @@ app.get('/', (req, res) => {
 
   let youtubers = [
     {videoAuthor: 'Marques Brownlee', videoTitle: 'Talking Tech and AI with Google CEO Sundar Pichai!',
-    videoStats: '3.4M views &#183; 6 months ago', videoTime: '14:20', totalSubs: '15.5M subscribers'},
+    videoStats: '3.4M views &#183; 6 months ago', videoTime: '14:20', totalSubs: '15.5M subscribers',
+    videoLink: 'https://www.youtube.com/watch?v=n2RNcPRtAiY', 
+    channelLink: 'https://www.youtube.com/channel/UCBJycsmduvYEL83R_U4JriQ'},
 
     {videoAuthor: 'Markiplier', videoTitle: 'Try Not To Laugh Challenge #9',
-    videoStats: '19M views &#183; 4 years ago', videoTime: '8:22', totalSubs: '32.6M subscribers'},
+    videoStats: '19M views &#183; 4 years ago', videoTime: '8:22', totalSubs: '32.6M subscribers',
+    videoLink: 'https://www.youtube.com/watch?v=mP0RAo9SKZk', 
+    channelLink: 'https://www.youtube.com/channel/UC7_YxT-KID8kRbqZo7MyscQ'},
 
     {videoAuthor: 'SSSniperWolf', videoTitle: 'Crazy Tik Toks Taken Moments Before DISASTER',
-    videoStats: '12M views &#183; 1 year ago', videoTime: '9:13', totalSubs: '31.4M subscribers'},
+    videoStats: '12M views &#183; 1 year ago', videoTime: '9:13', totalSubs: '31.4M subscribers',
+    videoLink: 'https://www.youtube.com/watch?v=FgjPQQeTh1w', 
+    channelLink: 'https://www.youtube.com/channel/UCpB959t8iPrxQWj7G6n0ctQ'},
 
     {videoAuthor: 'Veritsium', videoTitle: 'The Simplest Math Problem No One Can Solve - Collatz Conjecture',
-    videoStats: '18M views &#183; 4 months ago', videoTime: '22:09', totalSubs: '11.7M subscribers'},
+    videoStats: '18M views &#183; 4 months ago', videoTime: '22:09', totalSubs: '11.7M subscribers',
+    videoLink: 'https://www.youtube.com/watch?v=094y1Z2wpJg', 
+    channelLink: 'https://www.youtube.com/channel/UCHnyfMqiRRG1u-2MsSQLbXA'},
 
     {videoAuthor: 'CS Dojo', videoTitle: 'Kadane\'s Algorithm to Maximum Sum Subarray Problem',
-    videoStats: '519K views &#183; 5 years ago', videoTime: '11:17', totalSubs: '1.82M subscribers'},
+    videoStats: '519K views &#183; 5 years ago', videoTime: '11:17', totalSubs: '1.82M subscribers',
+    videoLink: 'https://www.youtube.com/watch?v=86CQq3pKSUw', 
+    channelLink: 'https://www.youtube.com/channel/UCxX9wt5FWQUAAz4UrysqK9A'},
 
     {videoAuthor: 'MrBeast', videoTitle: 'Anything You Can Fit In The Circle I’ll Pay For',
-    videoStats: '141M views &#183; 1 years ago', videoTime: '19:59', totalSubs: '93.6M subscribers'},
+    videoStats: '141M views &#183; 1 years ago', videoTime: '19:59', totalSubs: '93.6M subscribers',
+    videoLink: 'https://www.youtube.com/watch?v=yXWw0_UfSFg', 
+    channelLink: 'https://www.youtube.com/channel/UCX6OQ3DkcsbYNE6H8uQQuVA'},
     
   ]
 
